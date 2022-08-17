36eca34fb1126189c866c9ada36b31708e37f300
kleora86 <106878059+kleora86@users.noreply.github.com>
2022-08-17 06:55:24 +0300

    Р

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
     }
 
     guess() {
-
+        return Math.round((this.maxNumber + this.minNumber) / 2);
     }
 
     lower() {
-
+        this.maxNumber = Math.round((this.maxNumber + this.minNumber) / 2);
     }
 
     greater() {
-
+        this.minNumber = Math.round((this.maxNumber + this.minNumber) / 2);
     }
 }
 
